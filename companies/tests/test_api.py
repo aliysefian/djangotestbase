e6216aee504bb1f0c7569725c35531ec7e9f3388
ali <aliysefian@gmail.com>
2021-09-21 17:06:59 +0430

    add setup function

--- companies/tests/test_api.py
+++ companies/tests/test_api.py
@@ -12,10 +12,15 @@ import pytest
 
 @pytest.mark.django_db
 class TestGetCompanies(TestCase):
+
+
+    def setUp(self) -> None:
+        self.client=Client()
+        self.url=reverse('companies-list')
+        return super().setUp()
+
     def test_zero_companies_sholud_return_empty_list(self)->None:
-        client=Client()
-        url=reverse('companies-list')
-        response=client.get(url)
+        response=self.client.get(self.url)
         self.assertEqual(response.status_code,200)
         self.assertEqual(json.loads(response.content),[])
 
